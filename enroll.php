a156dde39af9e53ba6ff5cae042f7f622bd6df85
Diana Mocoh <mocohdiana04@gmail.com>
2022-07-04 14:02:33 +0300

    added enroll php

--- enroll.php
+++ enroll.php
@@ -1,49 +1,47 @@
 <?php
-//1/ database connection
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "zalego";
+$server="localhost";
+$username="root";
+$password="";
+$database="zalego";
 
-$conn = mysqli_connect($server,$username,$password,$database);
-
-if(isset($_POST ['submitButton'] ) )
+$conn= mysqli_connect($server,$username,$password,$database);
+if(isset($_POST["enroll"]))
 {
-  //fetch form data
-  $fullname = $_POST['fullname'];
-  $phonenumber = $_POST['phonenumber'];
-  $emailaddress =$_POST['emailaddress'];
-  $gender = $_POST['gender'];
-  $courses = $_POST['courses'];
-   //submit form data   
-  $insertData = mysqli_query($conn,"INSERT INTO 
-  registernow(fullname,phonenumber,emailaddress,gender,courses);
-  VALUES('$fullname','$phonenumber','$emailaddress','$gender','$courses') ");
+    $fullname= $_POST['fullname'];
+    $phonenumber= $_POST['phonenumber'];
+    $email= $_POST['email'];
+    $gender= $_POST['gender'];
+    $course= $_POST['course'];
 
-  if($insertData)
-  {
-    echo "Data";
-  } 
-  else
-  {
-    echo "Error occured";
-  }
+    $insertData =mysqli_query($conn, "INSERT INTO enrollment(fullname,phonenumber,email,gender,course)
+    VALUES('$fullname','$phonenumber','$email','$gender','$course')");
+
+    if($insertData)
+    {
+        echo "Data submitted successfully";
+    }
+    else{
+        echo "error occured";
+    }
+
 
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Register now</title>
-  <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
-  
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <title>Document</title>
 </head>
 <body>
-   <!-- Nav bar starts here -->
-   <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
+     <!-- Nav bar starts here -->
+     <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
         <div class="container-fluid">
             <a href="#" class="navbar-brand">Zalego Academy</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
@@ -52,8 +50,8 @@ if(isset($_POST ['submitButton'] ) )
             <div class="collapse navbar-collapse" id="menus">
                 <div class="navbar-nav">
                     <a href="#" class="nav-link active">Home</a>
-                    <a href="aboutus.html" class="nav-link">About us</a>
-                    <a href="#" class="btn btn-primary">Register Now</a>
+                    <a href="aboutus.html" class="nav-link ">About us</a>
+                    <a href="enrol.php" class="btn btn-primary">Register now</a>
 
                 </div>
 
@@ -61,82 +59,110 @@ if(isset($_POST ['submitButton'] ) )
         </div>
 
     </nav>
+
     <!-- Navbar ends -->
-    <main class="p-5 mb-4 bg-light">
-       <h1 style="padding-top: 30px; text-align:center ">JULY SOFTWARE ENGINEERING BOOTCAMP</h1>
-    <div class="container">
+    <main class=" text-center p-5 mb-4 bg-light">
+       <h1>JULY SOFTWARE ENGINEERING BOOTCAMP</h1>
+       <ul style="list-style-type:none ;">
+            <li>
+                   <span><i class="fa fa-calendar"></i></span>
+                   <span>20th July 2022</span>
+             </li>
+             <li>
+                   <span><i class="fa fa-map-marker"></i></span>
+                   <span>Zalego Academy <br>Devan plaza</span>
+             </li>
+       </ul>
+     </main>
     <div class="row">
-           <div class="col-lg-6">
-            <h1></h1>
+        <div class="col-lg-12 text-center">
      
-
-      </div>
-
-    </div>
-
-    <!-- Register now starts here -->
-    <div class="row pt-5">
-            
-            <p> Looking for a place to kickstart your career in Technology?
-              Whether you're a local,new in town or just cruising through we've got 
-                   loads of great tips and events for you. 
-
-                   
-            </p>
-            <p>
-              Sign up for today?
-
-            </p>
-
-            <form >
-                <div class="row">
-                    <div class="mb-3 col-lg-6">
-                        <label for="firstname" class="form-label">Fullname</label>
-                        <input type="text" class="form-control" placeholder="Enter your full name">
-                    </div>
-                    <div class="mb-3 col-lg-6">
-                        <label for="firstname" class="form-label">Phone number</label>
-                        <input type="tel" class="form-control" placeholder="Enter your phone number">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="mb-3 col-lg-6">
-                        <label for="firstname" class="form-label">Email address</label>
-                        <input type="email" class="form-control" placeholder="Enter your email address">
-                    </div>
-                    <div class="mb-3 col-lg-6">
-                        <label for="firstname" class="form-label">Gender</label>
-                        <input type="gender" class="form-control" placeholder="Select your gender">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="mb-3 col-lg-6">
-                        <label for="firstname" class="form-label">Courses</label>
-                        <input type="Courses" class="form-control" placeholder="Select your course">
-
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit application</button>
-                <p style="text-align:center; padding-top: 50px; color: gray;">Subscribe to get information,latest news about <br>Zalego Academy</p>
-            <div class="row pt-5">
-                <div class="col-lg-12" style="text-align:center;">
-                    <input type="email" name="email" placeholder="Enter your email address">
-                    <button class="btn btn-primary" name = "submitButton">Subscribe</button>
-            </form>
+        <p>Looking for a place to kickstart your career in technology?
+            Whether you're a local,new in town or just cruising through we've got
+                loads of great tips and events for you.
+        </p>
         </div>
-    <!-- end register now -->
+        </div>
+     <div class="card p-5">
+        <h3 class="text-center p-5">Sign up today ?</h3>
+        <form action="enroll.php" method="POST">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="fullname" class="form-label">Full name</label>
+                            <input type="text" class="form-control" name="fullname" placeholder="Enter your full name">
+                        </div>
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="phonenumber" class="form-label">Phone number</label>
+                            <input type="tel" class="form-control"name="phonenumber" placeholder="+2547...">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" name="email" placeholder="Please enter your email">
+                        </div>
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="gender" class="form-label">What's your gender</label>
+                            <select name="gender" class="form-select" aria-label="default select example">
+                                <option >--Select your gender--</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                        <p>In order to complete your registration to the bootcamp, you are required to select one course you will be 
+                            undertaking.Please NOTE that this will be your learning track during the 2-weeks immersion.
 
+                        </p>
+                        <div class="mb-3 col-lg-6">
+                            <select name="course" class="form-control multiplchose_questiontypes" id="selector">
+                                <option value="" >--choose your course--</option>
+                                <option value="web design">web design</option>
+                                <option value="cyber security">cyber security</option>
+                                <option value="android development">android development</option>
+
+                            </select>
+                         </div>
+                        <p>
+                        You agree by providing your information you understand all our data privacy protection policy
+                        outlined in our Terms & conditions and the privacy policy statements
+                    </p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                        <label class="form-check-label" for="flexCheckChecked">
+                           Agree terms and condition
+                        </label>
+                        </div>
+                    </div>           
+                 <button type="submit" name="enroll" class="btn btn-primary">submit application</button>
+            </div>
+        </form>
+                   
+ 
+            
+
+        
+     </div>
+     
+        <div class="mb-3 col-lg-6 col-md-6 col-sm-12" style="text-align: center;">
+            <p style="color:gray">Subscribe to get information, latest news about <br>Zalego Academy</p>
+        </div>
+        <div class="col-lg-12" style="text-align: center; margin-bottom: 3px">
+            <input type="email" placeholder="Your email address">
+            <button type="submit" name="" class="btn btn-primary">Subscribe</button>
+           </div>
+    
     <hr>
-    <footer>
-        &copy;Zalego Academy 2022
+    <footer class="align-items: center;">
+        &copy;Company 2022
     </footer>
     </div>
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <!-- <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script>
-    
-  
 </body>
 </html>
